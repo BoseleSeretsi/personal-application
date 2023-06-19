6a5270b1140a378068aa260802fc351037927db5
BoseleSeretsi <129956765+BoseleSeretsi@users.noreply.github.com>
2023-06-19 15:32:19 +0200

    message

--- FrontEnd/personal-app/src/app/auth-service/auth-service.component.ts
+++ FrontEnd/personal-app/src/app/auth-service/auth-service.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+
 
 @Component({
   selector: 'app-auth-service',
   templateUrl: './auth-service.component.html',
   styleUrls: ['./auth-service.component.css']
 })
+
 export class AuthServiceComponent {
 
 }
+
+
